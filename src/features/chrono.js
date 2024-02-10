67233fb86acb8c628bf9e6b72484feb759ccb6f3
Julien <newdevcreator@gmail.com>
2024-02-10 20:40:11 +0100

    FEAT: redux

---
+++ src/features/chrono.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+export const chrono = createSlice({
+  name: "chrono",
+  initialState,
+  reducers: {},
+});
+
+export default chrono.reducer;
