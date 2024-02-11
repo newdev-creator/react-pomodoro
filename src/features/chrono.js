7a6757b227507d891a1a4ab08ce081b412b25d28
Julien <newdevcreator@gmail.com>
2024-02-11 20:14:51 +0100

    FEAT: Update session and pause values

--- src/features/chrono.js
+++ src/features/chrono.js
@@ -21,7 +21,30 @@ const initialState = {
 export const chrono = createSlice({
   name: "chrono",
   initialState,
-  reducers: {},
+  reducers: {
+    updateChronoValues: (state, action) => {
+      const chosenState = state[action.payload.type];
+
+      // On block below 1
+      if (chosenState.value + action.payload.value === 0) return;
+
+      if (action.payload.type === "session") {
+        if (state.session.value === state.session.runningValue) {
+          chosenState.value = chosenState.value + action.payload.value;
+          chosenState.runningValue =
+            chosenState.runningValue + action.payload.value;
+          state.displayedValue.value = chosenState.runningValue;
+        } else {
+          chosenState.value = chosenState.value + action.payload.value;
+        }
+      }
+
+      if (action.payload.type === "pause") {
+        chosenState.value = chosenState.value + action.payload.value;
+      }
+    },
+  },
 });
 
+export const { updateChronoValues } = chrono.actions;
 export default chrono.reducer;
