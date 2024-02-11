f67f68795f67f9931d0f77ef75b256e4687cccf3
Julien <newdevcreator@gmail.com>
2024-02-11 22:00:33 +0100

    FEAT: thunk function

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { startChrono, resetChrono } from "../features/chrono";
+
 import play from "../assets/play-button.svg";
 import reset from "../assets/reset.svg";
 
 export default function ToggleButton() {
+  const dispatch = useDispatch();
+  const chronoValues = useSelector((state) => state.chrono);
+
+  function toggleChrono() {
+    if (!chronoValues.isPlaying) {
+      dispatch(startChrono());
+    } else {
+      dispatch(resetChrono());
+    }
+  }
+
   return (
-    <button className="px-4 py-2 text-slate-800 flex justify-center items-center mx-auto bg-slate-300 rounded hover:bg-slate-200">
-      <span className="mr-3 text-lg">Start</span>
-      <img className="w-5" src={play} alt="" />
+    <button
+      onClick={toggleChrono}
+      className="px-4 py-2 text-slate-800 flex justify-center items-center mx-auto bg-slate-300 rounded hover:bg-slate-200"
+    >
+      <span className="mr-3 text-lg">
+        {chronoValues.isPlaying ? "Reset" : "Start"}
+      </span>
+      <img className="w-5" src={chronoValues.isPlaying ? reset : play} alt="" />
     </button>
   );
 }
--- src/features/chrono.js
+++ src/features/chrono.js
@@ -43,8 +43,33 @@ export const chrono = createSlice({
         chosenState.value = chosenState.value + action.payload.value;
       }
     },
+
+    tick: (state, action) => {
+      console.log("TICK");
+    },
+
+    setUpChrono: (state, action) => {
+      state.isPlaying = true;
+      state.intervalID = action.payload;
+    },
+
+    resetChrono: (state, action) => {
+      window.clearInterval(state.intervalID);
+      state.isPlaying = false;
+    },
   },
 });
 
-export const { updateChronoValues } = chrono.actions;
+export function startChrono(action) {
+  return function (dispatch, getState) {
+    const intervalID = setInterval(() => {
+      dispatch(tick());
+    }, 1000);
+    dispatch(setUpChrono(intervalID));
+    dispatch(tick());
+  };
+}
+
+export const { updateChronoValues, setUpChrono, resetChrono, tick } =
+  chrono.actions;
 export default chrono.reducer;
